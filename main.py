53aa73be91b8f0fc7229023ab46fa4abac4c9d12
valeman10 <valerio.mannucci@tutored.it>
2023-03-27 17:56:23 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+import pandas as pd
+
+participants = ['Ilaria', 'Jody', 'Valerio', 'Stefania', 'Nicolo\'', 'Egidio', 'Gaia', 'Simone', 'Fabio', 'Davide',
+                'Emilia', 'Stefano', 'Vincenzo', 'Gabriele', 'Edel', 'Giuseppe', 'Isabel']
+
+# crate all the possible pairs
+teams = []
+for i in range(len(participants)):
+    for j in range(i + 1, len(participants)):
+        teams.append((participants[i], participants[j]))
+
+# shuffle the pairs
+random.shuffle(teams)
+matches = []
+
+i = 1
+while len(teams) >= 1:
+    team1 = teams[0]
+    team2 = teams[i]
+    if team1[0] in team2 or team1[1] in team2:
+        # print('Error')
+        i += 1
+    else:
+        matches.append((team1, team2))
+        if len(matches)==68:
+            break
+        teams.pop(0)
+        teams.pop(i-1)
+        i = 1
+
+matches.sort(key=lambda x: x[0][0])
+
+for i, match in enumerate(matches):
+    print(i, match)
+
+
+df = pd.DataFrame(matches)
+df.to_csv('matches_final.csv', index=False)
+
+# print the matches
+for i, match in enumerate(matches):
+    print(i, match)
+
+
+print('Hello, World!')
+
