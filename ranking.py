37b03d3096db58f5d771d17b4d37e4f80232184b
Jody Roberto Battistini <jodi.battistini@libero.it>
2023-05-07 13:44:56 +0200

    feat(): add participants.csv

--- ranking.py
+++ ranking.py
@@ -1,4 +1,5 @@
 import os
+
 import mysql.connector
 import pandas as pd
 from dotenv import load_dotenv
@@ -32,38 +33,68 @@ inner join matches m on m.user1 = u.name or m.user2 = u.name or m.user3 = u.name
 where m.score1 != 0 and m.score2 != 0
 ORDER BY score DESC
 '''
+
+    total_matches_query = '''SELECT count(*) FROM matches m
+where m.score1 != 0 or m.score2 != 0
+'''
+
     cursor.execute(table_query)
     table = cursor.fetchall()
+
+    cursor.execute(total_matches_query)
+    total_matches = cursor.fetchone()[0]
 
     df = pd.DataFrame(table, columns=['user_id', 'name', 'surname',
                                       'score', 'coefficient', 'match_id', 'user1', 'user2', 'user3', 'user4', 'score1',
                                       'score2'])
 
-participants = ['Ilaria', 'Jody', 'Valerio', 'Stefania', 'Nicolo\'', 'Egidio', 'Gaia', 'Simone', 'Fabio', 'Davide',
+participants = ['Ilaria', 'Jody', 'Valerio', 'Stefania', 'Nicolo', 'Egidio', 'Gaia', 'Simone', 'Fabio', 'Davide',
                 'Emilia', 'Stefano', 'Vincenzo', 'Gabriele', 'Edel', 'Giuseppe']
 score = {}
+match_played = {}
+matches_won = {}
 for name in participants:
     score_name = df[df.name == name]
     score[name] = 0
+    matches_won[name] = 0
+    match_played[name] = len(score_name)
     for line in score_name.iterrows():
         line = line[1]
         if name in line.user1 or name in line.user2:
             diff = abs(line.score2 - line.score1)
             if line.score1 >= 10:
                 score[name] += calculate_score(diff)
+                matches_won[name] += 1
             else:
                 score[name] -= calculate_score(diff) * line.coefficient
         else:
             diff = abs(line.score2 - line.score1)
             if line.score2 >= 10:
                 score[name] += calculate_score(diff)
+                matches_won[name] += 1
             else:
                 score[name] -= calculate_score(diff) * line.coefficient
+
 # sort the score using values
 score = {k: v for k, v in sorted(score.items(), key=lambda item: item[1], reverse=True)}
+
+# create a dataframe with the number of matches played
+matches_played = pd.DataFrame(match_played.items(), columns=['name', 'matches_played'])
+won = pd.DataFrame(matches_won.items(), columns=['name', 'matches_won'])
 
 # create a dataframe with the final score
 final_scores = pd.DataFrame(score.items(), columns=['name', 'score'])
 final_scores['score'] = final_scores['score'].apply(lambda x: round(x, 2))
+
+# merge the two dataframes
+final_scores = pd.merge(final_scores, matches_played, on='name')
+final_scores = pd.merge(final_scores, won, on='name')
+
+# add column with points per match
+final_scores['points_per_match'] = final_scores['score'] / final_scores['matches_played']
+final_scores['points_per_match'] = final_scores['points_per_match'].apply(lambda x: round(x, 2))
+
+print("Total matches played: " + str(total_matches))
 print(final_scores)
-print('done')
+
+cnx.close()
